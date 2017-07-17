f0a676eb156a79b7d8b37d3bf95e13d26a3a256a
jeevankc91 <jeevankc91@gmail.com>
2017-07-17 20:48:55 +0530

    updated script to get row and column length

--- Excell_Working.py
+++ Excell_Working.py
@@ -1,9 +1,17 @@
 import os
 import openpyxl
 
-os.chdir('F:\git_practice_dev1')
+os.chdir('F:\git_practice_dev1\Git_practice')
 wb = openpyxl.load_workbook('0WDAGAPP_F130D11_PIT_REPORT.xlsx')
 
 Sheet_name = wb.get_sheet_names()
-sheet = wb.get_sheet_by_name(Sheet_name[0])
+number_of_sheets = len(Sheet_name)
+my_sheet = 0
+for i in range(number_of_sheets):
+    if (Sheet_name[i] == 'UNUSED_VARIABLES'):
+        my_sheet = i
+sheet = wb.get_sheet_by_name(Sheet_name[my_sheet])
+row_count = sheet.max_row
+column_count = sheet.max_column
+print row_count,column_count
 print sheet.cell(row=1,column=1).value
